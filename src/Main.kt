1741214d81e4ccc5f45a5928d49885cd42893733
Luis Vilchez <Luis.vilchez@unicda.edu.do>
2025-06-02 09:13:12 -0400

    ejercicios aula

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.time.Duration.Companion.minutes
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
@@ -21,6 +23,41 @@ fun main() {
     println(1/2)
     println(1.3/2.0)
 
+
+    val fish: Int = 2
+    fish.times(6   )
+    fish.div(3)
+    fish.plus(5)
+    fish.minus(5)
+
+    //types
+    println("TYPES")
+
+
+    val i: Int = 6
+    val b1 = i.toByte()
+    println("bi is $b1" )
+
+    val b2: Byte = 1 // OK, literals are checked statically
+    println(b2)
+
+
+    val i1: Int = b2.toInt()
+    println(i1)
+
+
+    val i2: String = b2.toString()
+    println(i2)
+
+    val i3: Double = b2.toDouble()
+    println(i3)
+
+    //tips para hacer los datos mas leibles usando guion abajo
+
+    val oneMillion = 1_000_000
+    val socialSecurityNumber = 999_99_9999L
+    val hexBytes = 0xFF_EC_DE_5E
+    val bytes = 0b11010010_01101001_10010100_10010010
 
 }
 
---
+++ src/ejercicios_del_aula_1/Main2.kt
@@ -0,0 +1,93 @@
+package ejercicios_del_aula_1
+/*
+Exercise 1: Simple Age Checker 👶👴
+Declare a mutable variable to store a person's age.
+Assign an initial age to the variable.
+Write an if-else statement that prints:
+"Minor" if the age is less than 18.
+"Adult" if the age is 18 or greater.
+Reassign a different age to your variable and observe the output.
+*/
+fun main() {
+    var luis = 22
+    var sara = 15
+
+    if (luis < 18) {
+        println("luis es menor")
+    } else {
+        println("luis es mayor")
+    }
+    if (sara < 18) {
+        println("sara es menor")
+    } else {
+        println("sara es mayor")
+    }
+
+    /*Exercise 2: Grade Evaluator 🎓
+Declare an immutable variable to store a student's numerical grade (e.g., out of 100).
+Write a series of if-else if-else statements to print the letter grade based on the following scale:
+90-100: "A"
+80-89: "B"
+70-79: "C"
+60-69: "D"
+Below 60: "F"
+
+ */
+    val luisGrade = 95
+    if (luisGrade in 90..100) {
+        println("A")
+    } else if (luisGrade in 80..89) {
+        println("B")
+    } else if (luisGrade in 70..79) {
+        println("C")
+    } else if (luisGrade in 60..69) {
+        println("D")
+    } else {
+        println("F")
+    }
+    /*
+    Declare a mutable variable to store the current color of a traffic light (as a String, e.g., "Red", "Yellow", "Green").
+    Write a when statement (Kotlin's version of a switch statement) that prints the action a driver should take based on the light's color:
+    "Red": "Stop"
+    "Yellow": "Caution"
+    "Green": "Go"
+    Any other color: "Invalid light color"
+    Change the light color and see the different actions.
+*/
+    var trafficLight = "Green"
+
+    when (trafficLight)
+    {
+        "Green" -> println("Go")
+        "Red" -> println("Stop")
+        "Yellow" -> println("Caution")
+    }
+
+    /* Exercise 4: Even or Odd? 🤔
+Declare an immutable variable and initialize it with an integer.
+Write an if-else statement to determine if the number is even or odd.
+Print whether the number is "Even" or "Odd".
+Hint: You might need the modulo operator (%).
+
+     */
+
+    val x = 9
+
+    if (x % 2 == 0) {println("x es par" )}else {println("x es impar")}
+
+    /* Exercise 5: Login Attempt 🔑
+Declare two immutable String variables: savedUsername and savedPassword. Initialize them with some values.
+Declare two mutable String variables: enteredUsername and enteredPassword.
+Simulate a user entering a username and password by assigning values to enteredUsername and enteredPassword.
+Write an if statement with nested if statements (or use logical operators &&) to check:
+If enteredUsername matches savedUsername.
+If they match, then check if enteredPassword matches savedPassword.
+Print:
+"Login Successful" if both match.
+"Incorrect Password" if the username matches but the password doesn't.
+"Incorrect Username" if the username doesn't match.
+
+     */
+
+
+}
